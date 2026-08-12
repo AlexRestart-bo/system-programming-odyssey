54e66ac345c497ab8bfd66a8f5b80ee65e9382a2
alex_butterfly <alex.butterfly@beauty.com>
2026-08-12 13:44:29 -0400

    Закончена 3я задача. Обе функции работают правильно, однако рекурсивный способ (очевидно) намного дольше.

--- tasks/01-basics/task_003_fibonacci/src/main.cpp
+++ tasks/01-basics/task_003_fibonacci/src/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <cassert>
+#include <chrono>
 
 using std::cout;
+using std::cin;
+using std::endl;
+
+void testsFibonacci(void);
 
 int iter_fibonacci(int n){
+    if (n < 0) return 0;
     if (n == 0 || n == 1) return n;
     int out = 0;
     int previous = 0;
@@ -15,8 +22,49 @@ int iter_fibonacci(int n){
     return out;
 }
 
+int recurs_fibonacci(int n){
+    if (n <= 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return recurs_fibonacci(n-1) + recurs_fibonacci(n - 2);
+}
+
 int main(void){
-    cout << iter_fibonacci(5) << std::endl;
-    cout << iter_fibonacci(10) << std::endl;
+    cout << recurs_fibonacci(5) << std::endl;
+    cout << recurs_fibonacci(10) << std::endl;
+    testsFibonacci();
+    int num;
+    cout << "Enter a natural number" << std::endl;
+    std::cin >> num;
+    cout << "Fibonacci_" << num << " = " << iter_fibonacci(num) << std::endl; 
+    
     return 0;
+}
+
+void testsFibonacci(void){
+    cout << "Запуск тестов функций recurs_fibonacci() и iter_fibonacci()" << std::endl;
+    for (int i = -3; i < 40; i++){
+        assert(recurs_fibonacci(i) == iter_fibonacci(i));
+    }
+    /*
+    // подсчет времени выполнения функции recurs_fibonacci(40)
+    auto start_point = std::chrono::steady_clock::now();
+    recurs_fibonacci(40);
+    auto end_point = std::chrono::steady_clock::now();
+    auto duration_recurs = std::chrono::duration_cast<std::chrono::microseconds>(start_point - end_point);
+    long long recurs_time = duration_recurs.count();
+
+    // подсчет времени выполнения функции iter_fibonacci(40)
+    start_point = std::chrono::steady_clock::now();
+    iter_fibonacci(40);
+    end_point = std::chrono::steady_clock::now();
+    auto duration_iter = std::chrono::duration_cast<std::chrono::microseconds>(start_point - end_point);
+    long long iter_time = duration_iter.count();
+
+    cout << "recurs_time = " << recurs_time << " iter_time = " << iter_time << endl;
+    cout << "40е число Фибоначи рекурсивным способом подсчитывается в " << recurs_time / iter_time << "раз дольше" << endl;
+    */
+    cout << "Все тесты пройдены!" << std::endl;
 }
